e03dc98801a536f8ec5d764c12b4c628df3c8d54
ThanhLamHet <Thanhpro347@gmail.com>
2026-03-05 18:31:45 +0700

    Add files via upload

---
+++ Lab_3/Calculator.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Lab_3
+{
+	public partial class Calculator : Form
+	{
+		public Calculator()
+		{
+			InitializeComponent();
+		}
+
+
+	}
+}
---
+++ Lab_3/Form1.cs
@@ -0,0 +1,63 @@
+﻿namespace Lab_3
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private void btnCong_Click(object sender, EventArgs e)
+		{
+			double n = double.Parse(txtSoN.Text);
+			double m = double.Parse(txtSoM.Text);
+			double Tong = n + m;
+
+			txtKetQua.Text = Tong.ToString();
+		}
+
+		private void btnTru_Click(object sender, EventArgs e)
+		{
+			double n = double.Parse(txtSoN.Text);
+			double m = double.Parse(txtSoM.Text);
+			double Hieu = n - m;
+
+			txtKetQua.Text = Hieu.ToString();
+		}
+
+		private void btnNhan_Click(object sender, EventArgs e)
+		{
+			double n = double.Parse(txtSoN.Text);
+			double m = double.Parse(txtSoM.Text);
+			double Tich = n * m;
+
+			txtKetQua.Text = Tich.ToString();
+		}
+
+		private void btnChia_Click(object sender, EventArgs e)
+		{
+			double n = double.Parse(txtSoN.Text);
+			double m = double.Parse(txtSoM.Text);
+			double Thuong = n / m;
+			if (m == 0)
+			{
+				txtKetQua.Text = "Lỗi phép tính";
+			}
+			else
+			{
+				txtKetQua.Text = Thuong.ToString();
+			}
+
+		}
+
+		private void btnXoa_Click(object sender, EventArgs e)
+		{
+
+		}
+
+		private void btnThoat_Click(object sender, EventArgs e)
+		{
+
+		}
+	}
+}
